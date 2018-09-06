20e61ac12c81dd4d94a0a55e9c643462171cac99
tbscanlon <thomas.scanlon@outlook.com>
2018-09-06 19:57:26 +0100

    WIP getting unit testing in place

--- App.test.js
+++ App.test.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import App from './App'
+// import renderer from 'react-test-renderer'
 
-import renderer from 'react-test-renderer'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON()
-  expect(rendered).toBeTruthy()
+// require('react-native-mock/mock')
+
+configure({ adapter: new Adapter() })
+
+// it('renders without crashing', () => {
+//   const rendered = renderer.create(<App />).toJSON()
+//   expect(rendered).toBeTruthy()
+// })
+
+describe('Component: App', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  describe('Initialisation', () => {
+    it('Renders successfully', () => {
+      expect(wrapper.exists()).toBeTruthy()
+    })
+  })
 })
